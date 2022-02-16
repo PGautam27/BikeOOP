c63f5a29a1a3d127eca71f216140741cb7049250
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-02-16 22:54:05 +0530

    Bike codes

--- src/Bike.kt
+++ src/Bike.kt
@@ -1,3 +1,4 @@
+import kotlin.properties.Delegates
 
 enum class KeyState{
     ACCESSORYMODE,KEYOUT;
@@ -27,7 +28,7 @@ enum class ClutchState{
     }
 }
 enum class Indigator{
-    LEFT, RIGHT;
+    LEFT, RIGHT,NONE;
     companion object{
         fun IndigatorState(obj:Indigator):String{
             return if (obj.compareTo(LEFT) == 0){
@@ -56,27 +57,50 @@ enum class HeadlightState{
     }
 }
 internal var keyHolder = KeyState.KEYOUT
+//internal var cluthBar = ClutchState.NORMAL
+//internal var horns = HornState.RELEASED
+internal var indigat = Indigator.NONE
 class Bike {
     var key = keyHolder
+    var clutch = ClutchState.NORMAL
+    var brake = Brake.RELEASED
+    var horn = HornState.RELEASED
+    var indigator = indigat
+    lateinit var start:IgnitionState
+    var headLight = HeadlightState.LIGHTOFF
+    private var speed = 0
+    private var gear = 0
     init {
-        if (KeyState.KeySlot(key)){
-            println("Bike is now turned on")
+        println("OK cool")
+        var s:List<String> = readLine()!!.split(" ".toRegex())
+        if (s.contains("start")){
+            start = IgnitionState.IGNITION
+            println("hi bob")
         }
+        else
+            Return()
     }
-    private fun engine(){
+    fun gearChange(){
 
+    }
+    fun Return(){
+        return
     }
 }
 fun main() {
     var k:List<String> = readLine()!!.split(" ".toRegex())
-    var temp = false
-    val s = Bike()
+    if (k.contains("put")&&k.contains("key")){
+        keyHolder = KeyState.ACCESSORYMODE
+        Bike()
+    }
+    /*var temp = false
     for (i in k){
         if (i.lowercase()=="put"|| temp){
             temp = true
             if (i.lowercase()=="key"){
                 keyHolder = KeyState.ACCESSORYMODE
+                val s = Bike()
             }
         }
-    }
+    }*/
 }
