d52524d9b9b274c3b73fe9fde485ad29eec4c211
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-02-21 15:02:45 +0530

    Bike codes

--- src/Bike.kt
+++ src/Bike.kt
@@ -1,3 +1,7 @@
+import java.util.*
+import java.util.concurrent.Delayed
+import kotlin.concurrent.timer
+import kotlin.concurrent.timerTask
 import kotlin.properties.Delegates
 
 enum class KeyState{
@@ -9,7 +13,12 @@ enum class KeyState{
     }
 }
 enum class IgnitionState{
-    IGNITION
+    IGNITION;
+    companion object{
+        fun Started(obj:IgnitionState):Boolean{
+            return obj.compareTo(IGNITION)==0
+        }
+    }
 }
 enum class Brake{
     PRESSED, RELEASED;
@@ -22,7 +31,7 @@ enum class Brake{
 enum class ClutchState{
     NEUTRAL, NORMAL;
     companion object{
-        fun ClutchState(obj:ClutchState):Boolean{
+        fun CState(obj:ClutchState):Boolean{
             return obj.compareTo(NEUTRAL) == 0
         }
     }
@@ -51,8 +60,11 @@ enum class HornState{
 enum class HeadlightState{
     LIGHTON,LIGHTOFF;
     companion object{
-        fun ClutchState(obj:HeadlightState):Boolean{
-            return obj.compareTo(LIGHTON) == 0
+        fun LightState(obj:HeadlightState):String{
+            return if(obj.compareTo(LIGHTON) == 0){
+                LIGHTON.toString()
+            } else
+                LIGHTOFF.toString()
         }
     }
 }
@@ -62,28 +74,111 @@ internal var keyHolder = KeyState.KEYOUT
 internal var indigat = Indigator.NONE
 class Bike {
     var key = keyHolder
-    var clutch = ClutchState.NORMAL
-    var brake = Brake.RELEASED
-    var horn = HornState.RELEASED
-    var indigator = indigat
+    var clutch:ClutchState = ClutchState.NORMAL
+    var brake = {
+            c:Brake -> if (Brake.BrakeUsed(c)){
+        speed-=(speed/2)
+    }
+    }
+    var horn = {
+        d:HornState -> if (HornState.Horn(d) && KeyState.KeySlot(key) ){
+            println("Horn pressed")
+    }
+        else
+            println("Horn's left stay")
+    }
+    var fuel = 100
+    var indigator = {
+        i:Indigator -> println(Indigator.IndigatorState(i))
+    }
     lateinit var start:IgnitionState
-    var headLight = HeadlightState.LIGHTOFF
+    var headLight = {
+        h:HeadlightState -> println(HeadlightState.LightState(h))
+    }
     private var speed = 0
     private var gear = 0
+    private var speedLimit = 0
     init {
-        println("OK cool")
-        var s:List<String> = readLine()!!.split(" ".toRegex())
-        if (s.contains("start")){
-            start = IgnitionState.IGNITION
-            println("hi bob")
+        if(KeyState.KeySlot(key)){
+            var s:List<String> = readLine()!!.split(" ".toRegex())
+            if (s.contains("start")){
+                start = IgnitionState.IGNITION
+            }
+            else
+                Return()
         }
         else
             Return()
     }
-    fun gearChange(){
-
+   fun gearUpDown(gr:Int){
+        if (KeyState.KeySlot(key) && ClutchState.CState(clutch)){
+            if (gr==1 && gear >= 0 && gear <=4){
+                gear += 1
+                engine(gear)
+            }
+            else if (gr==-1 && gear >= 0 && gear <=4){
+                gear -=1
+                engine(gear)
+            }
+            else
+                return
+        }
     }
-    fun Return(){
+    private fun engine(gear:Int){
+        if(KeyState.KeySlot(key) && IgnitionState.Started(start)){
+            when(gear){
+                1 -> {
+                    speedLimit = 20
+                    fuel-=1
+                    fuelgaugemeter()
+                }
+                2 -> {
+                    speedLimit = 35
+                    fuel-=2
+                    fuelgaugemeter()
+                }
+                3 -> {
+                    speedLimit = 50
+                    fuel-=3
+                    fuelgaugemeter()
+                }
+                4 ->{
+                    speedLimit = 90
+                    fuel-=4
+                    fuelgaugemeter()
+                }
+            }
+        }
+    }
+    fun accelerator() {
+        when (gear) {
+            1 -> if (speedLimit<20){
+                speed += 5
+                speedometer()
+            }
+            2 -> if (speedLimit<35){
+                speed += 5
+                speedometer()
+            }
+            3 -> if (speedLimit<50){
+                speed += 5
+                speedometer()
+            }
+            4 -> if (speedLimit<90){
+                speed += 5
+                speedometer()
+            }
+            else -> return
+        }
+    }
+
+    private fun speedometer(){
+        println(speed)
+    }
+    private fun fuelgaugemeter(){
+        println(fuel)
+    }
+    private fun Return(){
         return
     }
 }
@@ -91,7 +186,10 @@ fun main() {
     var k:List<String> = readLine()!!.split(" ".toRegex())
     if (k.contains("put")&&k.contains("key")){
         keyHolder = KeyState.ACCESSORYMODE
-        Bike()
+        val d = Bike()
+        d.start
+        d.horn(HornState.PRESSED)
+        d.brake(Brake.PRESSED)
     }
     /*var temp = false
     for (i in k){
@@ -104,3 +202,4 @@ fun main() {
         }
     }*/
 }
+
